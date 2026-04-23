e1478e34172d21df7e84912ddba2fd374bfc71dd
okada <kd1348380@st.kobedenshi.ac.jp>
2026-04-23 12:08:59 +0900

    ログアウト時のモーダル追加

--- app/components/aikon.tsx
+++ app/components/aikon.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useState } from "react";
-import Link from "@/app/components/Link";
+import { useRouter } from "next/navigation";
 
 export default function Menu() {
   const [open, setOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const router = useRouter();
 
   return (
     <div className="fixed top-4 right-4">
@@ -14,11 +16,45 @@ export default function Menu() {
 
       {/* メニュー */}
       {open && (
-        <div className="absolute top-10 right-0 bg-white shadow p-4 flex gap-4 min-w-[120px]">
-          <Link href="/login">
-            ログアウト
-          </Link>
+        <div>
+      {/* ログアウトボタン */}
+      <button onClick={() => setShowModal(true)} className="absolute top-10 right-0 bg-blue-500 text-white shadow p-4 flex gap-4 min-w-[120px] flex items-center justify-center  hover:underline">
+        ログアウト
+      </button>
+
+      {/* モーダル */}
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black/50">
+          
+          <div className="bg-white p-6 rounded shadow w-80">
+            <p className="mb-4 text-center">
+              本当にログアウトしますか？
+            </p>
+
+            <div className="flex justify-end gap-2">
+              
+              {/* キャンセル */}
+              <button
+                onClick={() => setShowModal(false)}
+                className="px-5 py-2 bg-gray-200 rounded"
+              >
+                キャンセル
+              </button>
+
+              {/* ログアウト */}
+              <button
+                onClick={() => router.push("/login")}
+                className="px-4 py-2 bg-red-500 text-white rounded"
+              >
+                ログアウト
+              </button>
+
+            </div>
+          </div>
+
         </div>
+      )}
+    </div>
       )}
     </div>
   );
